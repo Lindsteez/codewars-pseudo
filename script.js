c9ef7e323277ba1d5437c0b85a73dbad06455ebf
Lindsteez <lindgren.axel@hotmail.com>
2025-09-24 10:02:59 +0200

    pseudo för students final grade

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mål - Räkna ut slutbetyg 
+// Inputs - Två parametrar, a grade for the exam och number of completed projects. Dom kommer vara i datatyp numbers
+// Return - Number och final grade
+// Funktionsbeskrivning - Vi ska skapa conditional statements för dessa regler:
+// Om betyget är över 90 eller om antal godkända projekt är över 10 ska return bli 100. 
+// Om betyget är högre än 75 och godkända projekt är minst 5 ska return bli 90. 
+// Om betyg är högre än 50 och om godkända projekt är minst 2 blir return 75.  
+// Om inget annan blir return 0.

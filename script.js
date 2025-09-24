604197b64ed78184d7eaeed98e60ed308251fc48
Lindsteez <lindgren.axel@hotmail.com>
2025-09-24 10:27:45 +0200

    pseudo för remove string spaces

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Pseudo för https://www.codewars.com/kata/5ad0d8356165e63c140014d4
+
 // Mål - Räkna ut slutbetyg 
 // Inputs - Två parametrar, a grade for the exam och number of completed projects. Dom kommer vara i datatyp numbers
 // Return - Number och final grade
@@ -6,3 +8,16 @@
 // Om betyget är högre än 75 och godkända projekt är minst 5 ska return bli 90. 
 // Om betyg är högre än 50 och om godkända projekt är minst 2 blir return 75.  
 // Om inget annan blir return 0.
+
+
+//Pseudo för https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+//Mål - Att ta bort alla mellanslang.
+//Inputs - En sträng.
+//Return - En modifierad sträng utan mellanslag.
+//Funktionsbeskrivning - 
+/* Steg 1. Kontrollera att input är en sträng.
+   Steg 2. Om NEJ = Prompta användare att input måste vara en sträng, eller konvertera input till en sträng. 
+   Steg 3. Om JA = Kontrollera om strängen innehåller whitespace.
+    Steg 3.A. Om JA = Ta bort whitespace och returnera modiefierad sträng. 
+    Steg 3.B. Om NEJ = Returna strängen som den är. */
